159cba92ba5ce6219b6937900f34525a66cf67f5
NewUser <mraflikurniawan20@gmail.com>
2025-06-30 15:11:41 +0700

    Fixing unused stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import CircleText from './components/CircleText';
-import SubTitle from './components/SubTitle';
-import Form from './components/Form';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 function App() {
-  // const handleSubmit = (data: Record<string, string>) => {
-  //   console.log(`Form Data: ${data}`);
-  // };
-  // const formInputs = [
-  //   { label: 'username', type: 'text', name: 'username', placeholder: 'username' },
-  //   { label: 'username', type: 'text', name: 'username', placeholder: 'username' },
-  // ];
   return (
     <>
-      {/* <CircleText />
-      <SubTitle subTitle="Create account Circle" />
-      <Form title="login" inputs={formInputs} onSubmit={handleSubmit} /> */}
       <RouterProvider router={router} />
     </>
   );
--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Command as CommandPrimitive } from 'cmdk';
-import { SearchIcon, User } from 'lucide-react';
+import { User } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
